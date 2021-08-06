9558875abeac198915ed34a21bd426f8216283ad
20047778 <20047778@LAPTOP-KBJ07VJL>
2021-08-06 19:02:37 +0800

    add, view abd delete user added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -5,11 +5,18 @@ public class C206_CaseStudy {
 	public static void main(String[] args) {
 
 		ArrayList<Category> categoryList = new ArrayList<Category>();
+		ArrayList<UserInfo> userAccList = new ArrayList<UserInfo>();
 		
 		categoryList.add(new Category("Stationery"));
 		categoryList.add(new Category("Laptop"));
+		
+		userAccList.add(new UserInfo("Ben","ben123", "Buyer", "ben@yahoo.com", "YH786nh"));
+		userAccList.add(new UserInfo("Candy", "canndy","Seller" ,"candy@gmail.com", "dvD567i"));
 
 		int option = 0;
+		
+		
+		mainMenu();
 
 		while (option != 4) {
 
@@ -34,7 +41,39 @@ public class C206_CaseStudy {
 			} else {
 				System.out.println("Invalid option");
 			}
+			mainMenu();
+		}
 
+	}
+	
+	private static void mainMenu() {
+		
+		//Write code here for the mainMenu method.
+		
+		Helper.line(60, "-");
+		System.out.println("CAMPUS ONLINE AUCTION SHOP"); 
+		Helper.line(60, "-");
+		System.out.println("1. Sign Up");
+		System.out.println("2. Log in");
+		System.out.println("4. Quit");
+		
+		int option = 0;
+		while (option != 4) {
+
+			menu();
+			option = Helper.readInt("Enter an option > ");
+
+			if (option == 1) {
+				addUser(userAccList);
+
+			} else if (option == 2) {
+				
+
+
+			} else {
+				System.out.println("Invalid option");
+			}
+			mainMenu();
 		}
 
 	}
@@ -117,5 +156,69 @@ public class C206_CaseStudy {
 		System.out.println(removeCat + " is removed"); 
 
 	}
+	
+	
+	public static void addUser(ArrayList<UserInfo> userAccList, UserInfo ua) {
+		
+		Helper.line(50, "-");
+		System.out.println("Sign Up");
+		Helper.line(50, "-");
+		
+		String newName = Helper.readString("Enter name > ");
+		String newUsername = Helper.readString("Enter username > ");
+		String newRole = Helper.readString("Enter role > ");
+		String newEmail = Helper.readString("Enter email > ");
+		String newPassword = Helper.readString("Enter password > ");
+		
+		boolean isSame = false;
+		
+		for (int i=0; i < userAccList.size(); i++) {
+			if(newUsername.equalsIgnoreCase(userAccList.get(i).getUsername())) {
+				isSame = true;
+				System.out.println("Username exist!");
+			}
+		}
+			if (isSame == false) {
+				userAccList.add(new UserInfo( newName,newUsername,newRole, newEmail, newPassword));
+			}
+	}
+
+	
+	public static String viewAllUser(ArrayList<UserInfo> userAccList) {
+		
+		Helper.line(50, "-");
+		System.out.println("View All User");
+		Helper.line(50, "-");
+		
+			if (userAccList.isEmpty() == false) { 
+			
+				String output = String.format("%s\n", "ALL USERS"); 
+			
+			for (int i = 0; i < userAccList.size(); i++) {
+
+				output += String.format("%s\n", userAccList.get(i).getName(), userAccList.get(i).getEmail(), userAccList.get(i).getRole());
+		}
+			
+			System.out.println(output);
+		}
+	}
+
+	
+
+	public static void deleteUser(ArrayList<UserInfo> userAccList) {
+		
+		Helper.line(50, "-");
+		System.out.println("Delete User");
+		Helper.line(50, "-");
+		
+		String delUserEmail = Helper.readString("Enter email to delete user");
+		
+		if (delUserEmail.contains("@")) {
+			
+		}
+		
+	}
+	
+	
 }
 
