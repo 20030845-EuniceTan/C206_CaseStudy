623eca73e7ebd8c44848e735bda63c3be163694b
20047778 <20047778@LAPTOP-KBJ07VJL>
2021-08-06 21:52:29 +0800

    add, view and delete user added

--- C206_CaseStudy/src/C206_CaseStudy.java
+++ C206_CaseStudy/src/C206_CaseStudy.java
@@ -4,19 +4,15 @@ public class C206_CaseStudy {
 
 	public static void main(String[] args) {
 
-		ArrayList<Category> categoryList = new ArrayList<Category>();
-		ArrayList<UserInfo> userAccList = new ArrayList<UserInfo>();
+
+		ArrayList<UserAccount> userAccList = new ArrayList<UserAccount>();
 		
-		categoryList.add(new Category("Stationery"));
-		categoryList.add(new Category("Laptop"));
 		
-		userAccList.add(new UserInfo("Ben","ben123", "Buyer", "ben@yahoo.com", "YH786nh"));
-		userAccList.add(new UserInfo("Candy", "canndy","Seller" ,"candy@gmail.com", "dvD567i"));
+		userAccList.add(new UserAccount("Ben","ben123", "Buyer", "ben@yahoo.com", "YH786nh"));
+		userAccList.add(new UserAccount("Candy", "canndy","Seller" ,"candy@gmail.com", "dvD567i"));
 
 		int option = 0;
 		
-		
-		mainMenu();
 
 		while (option != 4) {
 
@@ -25,15 +21,16 @@ public class C206_CaseStudy {
 
 			if (option == 1) {
 				// add item
-				addCategory(categoryList);
+				UserAccount user = inputUser(); 
+				C206_CaseStudy.addUser(userAccList, user);
 
 			} else if (option == 2) {
 				// view all item
-				viewAllCategory(categoryList);
+				C206_CaseStudy.viewAllUser(userAccList);
 
 			} else if (option == 3) {
 				// delete an item
-				deleteCategory(categoryList);
+				C206_CaseStudy.deleteUser(userAccList);
 
 			} else if (option == 4) {
 				System.out.println("Bye!");
@@ -41,48 +38,18 @@ public class C206_CaseStudy {
 			} else {
 				System.out.println("Invalid option");
 			}
-			mainMenu();
+			
 		}
 
 	}
 	
-	private static void mainMenu() {
+
 		
-		//Write code here for the mainMenu method.
-		
-		Helper.line(60, "-");
-		System.out.println("CAMPUS ONLINE AUCTION SHOP"); 
-		Helper.line(60, "-");
-		System.out.println("1. Sign Up");
-		System.out.println("2. Log in");
-		System.out.println("4. Quit");
-		
-		int option = 0;
-		while (option != 4) {
-
-			menu();
-			option = Helper.readInt("Enter an option > ");
-
-			if (option == 1) {
-				addUser(userAccList);
-
-			} else if (option == 2) {
-				
-
-
-			} else {
-				System.out.println("Invalid option");
-			}
-			mainMenu();
-		}
-
-	}
-
 	public static void menu() {
 		C206_CaseStudy.setHeader("CAMPUS ONLINE AUCTION SHOP");
-		System.out.println("1. Add Category");
-		System.out.println("2. View All Categories");
-		System.out.println("3. Delete Category");
+		System.out.println("1. Add User");
+		System.out.println("2. View All User");
+		System.out.println("3. Delete User");
 		System.out.println("4. Quit");
 		Helper.line(80, "-");
 
@@ -94,74 +61,10 @@ public class C206_CaseStudy {
 		Helper.line(80, "-");
 	}
 
-	public static void addCategory(ArrayList<Category> categoryList) {
+	public static UserAccount inputUser() {
 		
 		Helper.line(50, "-");
-		System.out.println("ADD CATEGORY");
-		Helper.line(50, "-");
-
-		String newName = Helper.readString("Enter new category name > ");
-		
-		boolean isSame = false; 
-		
-		for (int i = 0; i < categoryList.size(); i++) {
-			if(newName.equalsIgnoreCase(categoryList.get(i).getName())) { 
-				isSame = true; 
-				System.out.println("This category already exists!"); 
-			}
-		}
-		
-		if (isSame == false) { 
-			
-			categoryList.add(new Category(newName));
-			System.out.println("New Category Added");
-				
-		}
-	}
-
-	public static void viewAllCategory(ArrayList<Category> categoryList) {
-		
-		Helper.line(50, "-");
-		System.out.println("VIEW ALL CATEGORIES");
-		Helper.line(50, "-");
-		
-		if (categoryList.isEmpty() == false) { 
-			
-			String output = String.format("%s\n", "CATEGORIES AVAILABLE"); 
-			
-			for (int i = 0; i < categoryList.size(); i++) {
-
-				output += String.format("%s\n", categoryList.get(i).getName());
-		}
-			
-			System.out.println(output);
-		}
-	}
-
-	public static void deleteCategory(ArrayList<Category> categoryList) {
-		
-		Helper.line(50, "-");
-		System.out.println("DELETE CATEGORY");
-		Helper.line(50, "-");
-		
-		String removeCat = Helper.readString("Enter category to delete > "); 
-		
-		for (int i = 0; i < categoryList.size(); i++) { 
-			if (categoryList.get(i).getName().equalsIgnoreCase(removeCat)) { 
-				categoryList.remove(i); 
-				break;
-			}
-		}
-		
-		System.out.println(removeCat + " is removed"); 
-
-	}
-	
-	
-	public static void addUser(ArrayList<UserInfo> userAccList, UserInfo ua) {
-		
-		Helper.line(50, "-");
-		System.out.println("Sign Up");
+		System.out.println("Add User");
 		Helper.line(50, "-");
 		
 		String newName = Helper.readString("Enter name > ");
@@ -170,53 +73,85 @@ public class C206_CaseStudy {
 		String newEmail = Helper.readString("Enter email > ");
 		String newPassword = Helper.readString("Enter password > ");
 		
+		UserAccount user = new UserAccount(newName, newUsername, newRole, newEmail, newPassword);
+		
+		return user;
+	}
+	
+	
+	public static void addUser(ArrayList<UserAccount> userAccList, UserAccount user) {
+		
 		boolean isSame = false;
 		
 		for (int i=0; i < userAccList.size(); i++) {
-			if(newUsername.equalsIgnoreCase(userAccList.get(i).getUsername())) {
+			if(userAccList.get(i).getUsername().equalsIgnoreCase(user.getUsername())) {
 				isSame = true;
-				System.out.println("Username exist!");
+				System.out.println("Username already exist!");
 			}
 		}
 			if (isSame == false) {
-				userAccList.add(new UserInfo( newName,newUsername,newRole, newEmail, newPassword));
+				userAccList.add(user);
+				System.out.println("Username added!");
 			}
 	}
 
+	//retrieve method
+	public static String retrieveAllUser(ArrayList<UserAccount> userAccList) {
+		
+		String output = ""; 
+		 
+		for (int i = 0; i < userAccList.size(); i++) {
+
+			output += String.format("%-15s %-15s %-15s %-15s %-15s\n", userAccList.get(i).getName(), userAccList.get(i).getUsername(), userAccList.get(i).getRole(), userAccList.get(i).getEmail(), userAccList.get(i).getPassword());
+	}
+	 return output; 
+	}
 	
-	public static String viewAllUser(ArrayList<UserInfo> userAccList) {
+	public static void viewAllUser(ArrayList<UserAccount> userAccList) {
 		
 		Helper.line(50, "-");
 		System.out.println("View All User");
 		Helper.line(50, "-");
 		
-			if (userAccList.isEmpty() == false) { 
+		String output = String.format("%-15s %-15s %-15s %-15s %-15s\n", "NAME", "USER NAME", "ROLE", "EMAIL", "PASSWORD"); 
 			
-				String output = String.format("%s\n", "ALL USERS"); 
+		output += retrieveAllUser(userAccList); 
 			
-			for (int i = 0; i < userAccList.size(); i++) {
-
-				output += String.format("%s\n", userAccList.get(i).getName(), userAccList.get(i).getEmail(), userAccList.get(i).getRole());
-		}
-			
-			System.out.println(output);
-		}
+		System.out.println(output);
+		
 	}
 
 	
 
-	public static void deleteUser(ArrayList<UserInfo> userAccList) {
+	public static void deleteUser(ArrayList<UserAccount> userAccList) {
 		
 		Helper.line(50, "-");
 		System.out.println("Delete User");
 		Helper.line(50, "-");
 		
-		String delUserEmail = Helper.readString("Enter email to delete user");
+		String delUserEmail = Helper.readString("Enter email to delete user >");
+		Boolean delUser = doDeleteUser(userAccList, delUserEmail);
 		
-		if (delUserEmail.contains("@")) {
+		if (delUser == false) {
+			System.out.println("Invalid category");
 			
+		} else {
+			System.out.println(delUserEmail + " is removed");
 		}
+	}
 		
+
+	public static boolean doDeleteUser(ArrayList<UserAccount> userAccList, String delUserEmail) {
+		
+		boolean delUser = false;
+		
+		for (int i = 0; i< userAccList.size(); i++) {
+			if (userAccList.get(i).getEmail().equalsIgnoreCase(delUserEmail)) {
+				userAccList.remove(i);
+				delUser = true;
+			}
+		}
+		return delUser;
 	}
 	
 	
